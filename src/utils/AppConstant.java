a2ca34990ce04d15c1a0cbfb66bce35b8f74de92
HackMemory0 <dragon.warri0r@ya.ru>
2020-06-09 03:46:35 +0300

    Revert "Delete AppConstant.java"
    This reverts commit 9514a471edba99f0ddce8f0e33da8c4e14239fd4.

---
+++ src/utils/AppConstant.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class AppConstant {
+    public final static int DEFAULT_PORT = 5454;
+    public final static String CONNECT_URL = "localhost";
+    public static final int MESSAGE_BUFFER = 65565;
+
+    public static final String FILE_PATH = System.getenv("PROG_PATH");
+
+    public final static String VERSION = "0.3";
+
+    public final static String DB_URL = "jdbc:postgresql://pg:5432/studs";
+    public final static String DB_USER = "s289136";
+    public final static String DB_PASS = "fbb581";
+
+}
